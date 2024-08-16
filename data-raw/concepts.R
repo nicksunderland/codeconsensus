1d79767e16cc964f19fe1bb6d942a46df25998e4
Nicholas Sunderland <nicholas.p.sunderland@gmail.com>
2024-08-16 09:24:45 +0100

    CPT4 updates

--- data-raw/concepts.R
+++ data-raw/concepts.R
@@ -35,7 +35,7 @@ configs <- configs[!file.info(configs)$isdir & !configs %in% project_files]
 # options(future.globals.maxSize = 90 * 1024^3)  # 2 GiB
 # future_walk(configs, function(config) {
 
-for (config in configs) {
+for (config in configs[29]) {
 
   library(hfphenotyping)
   library(Rdiagnosislist)
